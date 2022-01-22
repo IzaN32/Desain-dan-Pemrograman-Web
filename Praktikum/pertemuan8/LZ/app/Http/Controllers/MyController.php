d567959f615e16754745ca8347a2e0013bb4d37f
Rize Hersch <34876769+IzaN32@users.noreply.github.com>
2022-01-23 01:33:53 +0700

    P8

--- Praktikum/pertemuan8/LZ/app/Http/Controllers/MyController.php
+++ Praktikum/pertemuan8/LZ/app/Http/Controllers/MyController.php
@@ -2,18 +2,26 @@
 
 namespace App\Http\Controllers;
 
-use Facade\FlareClient\View;
 use Illuminate\Http\Request;
 
 class MyController extends Controller
 {
     // function untuk mengakses beranda
-    public function index(){
+    public function index()
+    {
         return view('beranda');
     }
 
     // function untuk mengakses beranda
-    public function about(){
+    public function about()
+    {
         return view('about');
+    }
+
+
+    // function untuk mengakses beranda
+    public function mahasiswa()
+    {
+        return view('mahasiswa');
     }
 }
